5136242468c559260f22b8372596ed946152e769
thallesnct <thallescarvalho12@gmail.com>
2021-10-30 17:56:07 -0300

    fix: vertex_a and vertex_b variable names

--- graph.py
+++ graph.py
@@ -8,9 +8,9 @@ import datetime
 def calculate_adjancency_list(graph):
   adjancency_list = { vertex: set() for vertex in graph['vertices'] }
 
-  for (vertexA, vertexB) in graph['edges']:
-    adjancency_list[vertexA].add(vertexB)
-    adjancency_list[vertexB].add(vertexA)
+  for (vertex_a, vertex_b) in graph['edges']:
+    adjancency_list[vertex_a].add(vertex_b)
+    adjancency_list[vertex_b].add(vertex_a)
 
   return adjancency_list
 
