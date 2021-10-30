b131dde9ab0cd046bb01aa3eb18932ff0a41fa26
thallesnct <thallescarvalho12@gmail.com>
2021-10-30 17:52:28 -0300

    add: find cycles by traversal

---
+++ graph.py
@@ -0,0 +1,69 @@
+# Grafo exemplo
+# graph = {
+#   'vertices': [1, 2, 3, 4, 5],
+#   'edges': ((1,3), (3,2), (4,5), (5,1), (2,4))
+# }
+import datetime
+
+def calculate_adjancency_list(graph):
+  adjancency_list = { vertex: set() for vertex in graph['vertices'] }
+
+  for (vertexA, vertexB) in graph['edges']:
+    adjancency_list[vertexA].add(vertexB)
+    adjancency_list[vertexB].add(vertexA)
+
+  return adjancency_list
+
+def add_vertex_to_path(vertex, path):
+  new_path = path.copy()
+  new_path.append(vertex)
+
+  return new_path
+
+def dfs(vertex, adjancency_list, visited, all_cycles, path = []):
+  if (visited.get(vertex) is True):
+    if (vertex is path[0] and len(path) >= 2):
+      all_cycles.append(add_vertex_to_path(vertex, path))
+    return
+
+  visited[vertex] = True
+  new_path = add_vertex_to_path(vertex, path)
+
+  for child in adjancency_list[vertex]:
+    dfs(child, adjancency_list, visited, all_cycles, new_path)
+
+def find_cycles_by_traversal(graph):
+  adjancency_list = calculate_adjancency_list(graph)
+
+  all_cycles = list()
+
+  for vertex in graph['vertices']:
+    visited = dict()
+
+    dfs(vertex, adjancency_list, visited, all_cycles)
+
+  return all_cycles  
+
+def main():
+  graph = {
+    'vertices': [1, 2, 3, 4, 5],
+    'edges': ((1,3), (3,2), (4,5), (5,1), (2,4))
+  }
+
+  start_time = datetime.datetime.now()
+
+  cycles = find_cycles_by_traversal(graph)
+
+  end_time = datetime.datetime.now()
+
+  time_diff = (end_time - start_time)
+
+  execution_time = time_diff.total_seconds() * 1000
+
+
+  print(cycles)
+  print(f'took {execution_time}ms to run find_all_cycles_by_traversal')
+
+if __name__ == "__main__":
+   # Runs main if file is executed
+   main()
